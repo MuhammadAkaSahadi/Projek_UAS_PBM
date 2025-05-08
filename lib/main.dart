94e2a115aa521421e83fb95ab9363a060d91a91a
Muhammad Aka Sahadi <makasahadi14@gmail.com>
2025-05-08 21:45:50 +0700

    Simpan sementara sebelum rebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:projek_uas/screen/beranda.dart';
+import 'package:projek_uas/screen/kebunSaya.dart';
+import 'package:projek_uas/screen/hasilLaporan.dart';
+import 'package:projek_uas/screen/akun.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
@@ -10,62 +17,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Pocket Farm',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'FarmCare'),
+      home: const MyHomePage(title: 'Pocket Farm'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
-  @override 
+  @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  // Daftar widget halaman
+  final List<Widget> _pages = [
+    Beranda(),
+    KebunSaya(),
+    HasilLaporan(),
+    Akun()
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              _selectedIndex == 0
+                  ? 'assets/Beranda_hitam.png'
+                  : 'assets/Beranda.png',
+              width: 20,
+              height: 20,
             ),
-          ],
-        ),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              _selectedIndex == 1
+                  ? 'assets/Kebun Saya_hitam.png'
+                  : 'assets/Kebun Saya.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'Kebun Saya',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              _selectedIndex == 2
+                  ? 'assets/Hasil Laporan_hitam.png'
+                  : 'assets/Hasil Laporan.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'Hasil Laporan',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              _selectedIndex == 3
+                  ? 'assets/Akun_hitam.png'
+                  : 'assets/Akun.png',
+              width: 20,
+              height: 20,
+            ),
+            label: 'Akun',
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
---
+++ lib/screen/kebunSaya.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KebunSaya extends StatelessWidget {
+  const KebunSaya({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Ini Kebun Saya"));
+  }
+}
