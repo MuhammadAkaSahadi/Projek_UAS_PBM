6d138f8dc5245458f3d58179cc23830a2058b48b
Muhammad Aka Sahadi <makasahadi14@gmail.com>
2025-05-13 14:02:39 +0700

    Penambahan Beranda

---
+++ lib/screen/detail/detailPanen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPanen extends StatefulWidget {
+  const DetailPanen({super.key});
+
+  @override
+  State<DetailPanen> createState() => _DetailPanenState();
+}
+
+class _DetailPanenState extends State<DetailPanen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
